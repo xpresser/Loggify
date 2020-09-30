498eddb86a45056e3535955c09cae31b9960dfb3
Acer <silvia@denchsolutions.com>
2020-09-30 19:33:56 +0300

    Added MainCOntent wrapper and imported bootstrap css

--- frontend/loggingsystem/src/App.js
+++ frontend/loggingsystem/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MainContent } from "./components/layout/MainContent/MainContent";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div id="appbar">
-          <div id="maincontent">
+          <MainContent>
             <AppRoutes />
-          </div>
+          </MainContent>
         </div>
       </BrowserRouter>
     </div>
