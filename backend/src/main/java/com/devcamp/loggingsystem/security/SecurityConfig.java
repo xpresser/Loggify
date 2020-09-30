fbdf70696572bcb1e72a047be85143fb16b82a70
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-09-30 19:24:49 +0300

    MD02-1870: Create Security Configuration

---
+++ backend/src/main/java/com/devcamp/loggingsystem/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.devcamp.loggingsystem.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author Metodi Vladimirov
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .mvcMatchers("/v1/auth/register", "/v1/auth/signin", "/swagger-ui.html","webjars/*").permitAll()
+                .anyRequest().permitAll();
+    }
+}
