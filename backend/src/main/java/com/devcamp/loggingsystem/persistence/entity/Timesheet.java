19873c4d7c8d3251f830844ac12f9f561eb633ad
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-09-30 18:45:32 +0300

    MD02-1870: Remove redundant import

--- backend/src/main/java/com/devcamp/loggingsystem/persistence/entity/Timesheet.java
+++ backend/src/main/java/com/devcamp/loggingsystem/persistence/entity/Timesheet.java
@@ -7,7 +7,6 @@ import org.springframework.validation.annotation.Validated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
