b864dd47e0708d00b7a93bf812671dc90a498ac1
Acer <silvia@denchsolutions.com>
2020-09-30 18:56:28 +0300

    Added login page

--- frontend/loggingsystem/src/pages/Auth/LoginPage.js
+++ frontend/loggingsystem/src/pages/Auth/LoginPage.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { Formik, Form } from "formik";
+import { AuthTitle } from "../../components/generic/AuthTitle/AuthTitle.styled";
+import { LoginValidationSchema } from "../../validations/schemas/login";
+import { TextInputField } from "../../components/generic/TextInputField/TextInputField";
 
-const LoginPage = () => {};
+const LoginPage = () => {
+  return (
+    <Card>
+      <AuthTitle>Login</AuthTitle>
+      <Card.Body>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={() => {
+            console.log("submitted");
+          }}
+          validationSchema={LoginValidationSchema}
+        >
+          {({ isValid }) => {
+            return (
+              <Form>
+                <TextInputField name="username" label="Username" />
+                <TextInputField
+                  name="password"
+                  type="password"
+                  label="Password"
+                />
+                <Button disabled={!isValid} type="submit">
+                  Login
+                </Button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export { LoginPage };
