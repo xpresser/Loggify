802a464779100bc7c6af95b6ebba254ac459d2ad
Acer <silvia@denchsolutions.com>
2020-09-30 18:53:52 +0300

    Added AppRoutes and route to /login

--- frontend/loggingsystem/src/App.js
+++ frontend/loggingsystem/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./AppRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div id="appbar">
+          <div id="maincontent">
+            <AppRoutes />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
