3a411fe5ca72b8e7d65eef62a7b7c45ae3d16181
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-09-30 19:48:48 +0300

    MD02-1873: Create custom ResourceNotFound exception

---
+++ backend/src/main/java/com/devcamp/loggingsystem/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.devcamp.loggingsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Custom exception for not found resource/entity in database
+ * @author Metodi Vladimirov
+ */
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found in database")
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
