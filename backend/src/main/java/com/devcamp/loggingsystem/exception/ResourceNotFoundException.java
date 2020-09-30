6ea1a2b8040c376cb91837e5129ffc629b2afe0e
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-09-30 20:13:54 +0300

    MD02-1873: Add empty line

--- backend/src/main/java/com/devcamp/loggingsystem/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/devcamp/loggingsystem/exception/ResourceNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Custom exception for not found resource/entity in database
+ *
  * @author Metodi Vladimirov
  */
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found in database")
