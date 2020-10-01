5aeefea80195f386a419250b120bdbc03ef02936
Acer <silvia@denchsolutions.com>
2020-10-01 10:23:27 +0300

    Added navigation bar

--- frontend/loggingsystem/src/AppRoutes.js
+++ frontend/loggingsystem/src/AppRoutes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Switch from "react-bootstrap/esm/Switch";
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { LoginPage } from "./pages/Auth/LoginPage";
 
 const AppRoutes = () => {
   return (
     <Switch>
       <Route path="/login" exact component={LoginPage} />
+      <Redirect to="/login" />
     </Switch>
   );
 };
