6bc079bcdce478b2e7d45c0253b57e47b08a165c
speeddemon <mikael90@abv.bg>
2020-10-01 16:43:36 +0300

    Added endpoint for getting user by id

---
+++ backend/src/main/java/com/devcamp/loggingsystem/exception/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package com.devcamp.loggingsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author - Mikael Parsekyan
+ */
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found in database")
+public class UserNotFoundException extends RuntimeException {
+
+    private static final String EXCEPTION_MESSAGE = "User not found!";
+
+    public UserNotFoundException() {
+        super(EXCEPTION_MESSAGE);
+    }
+}
