2f171835c902483ecdbdeecb5decc3bbd3b826e9
Acer <silvia@denchsolutions.com>
2020-10-01 18:09:56 +0300

    Temp changes to these files

--- frontend/loggingsystem/src/pages/Auth/LoginPage.js
+++ frontend/loggingsystem/src/pages/Auth/LoginPage.js
@@ -6,14 +6,14 @@ import { LoginValidationSchema } from "../../validations/schemas/login";
 import { TextInputField } from "../../components/generic/TextInputField/TextInputField";
 import { LoginRedirect } from "../../components/generic/redirects/login/LoginRedirect";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../../store/slices/auth";
+// import { login } from "../../store/slices/auth";
 import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
-  const error = useSelector((state) => state.auth.error);
-  const isLoading = useSelector((state) => state.auth.isLoading);
-  const dispatch = useDispatch();
-  const history = useHistory();
+  // const error = useSelector((state) => state.auth.error);
+  // const isLoading = useSelector((state) => state.auth.isLoading);
+  // const dispatch = useDispatch();
+  // const history = useHistory();
 
   return (
     <Card>
@@ -22,8 +22,8 @@ const LoginPage = () => {
         <Formik
           initialValues={{ username: "", password: "" }}
           onSubmit={async (values) => {
-            await dispatch(login(values));
-            history.push("/");
+            // await dispatch(login(values));
+            // history.push("/");
 
             console.log("submitted");
           }}
@@ -32,7 +32,7 @@ const LoginPage = () => {
           {({ isValid }) => {
             return (
               <Form>
-                {error && <Alert variant="danger">{error?.message}</Alert>}
+                {/* {error && <Alert variant="danger">{error?.message}</Alert>} */}
                 <TextInputField name="username" label="Username" />
                 <TextInputField
                   name="password"
@@ -40,7 +40,7 @@ const LoginPage = () => {
                   label="Password"
                 />
                 <LoginRedirect />
-                <Button disabled={!isValid || isLoading} type="submit">
+                <Button disabled={!isValid} type="submit">
                   Login
                 </Button>
               </Form>
