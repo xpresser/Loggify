e8cefd15f1f05d861f4bea88a1683f54905d5fc2
Acer <silvia@denchsolutions.com>
2020-10-02 22:13:57 +0300

    Added button access control in navbar

--- frontend/loggingsystem/src/components/layout/AppBar.js
+++ frontend/loggingsystem/src/components/layout/AppBar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../store/slices/auth";
 
 const AppBar = () => {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
   return (
     <Navbar bg="dark" variant="dark" fixed="top" expand="sm">
       <Navbar.Brand as={NavLink} to="/">
@@ -11,26 +16,38 @@ const AppBar = () => {
 
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link eventKey="1" as={NavLink} to="/timesheets" exact>
-            All timesheets
-          </Nav.Link>
+        {user ? (
+          <Nav className="ml-auto">
+            <Nav.Link eventKey="1" as={NavLink} to="/timesheets" exact>
+              All timesheets
+            </Nav.Link>
 
-          <Nav.Link eventKey="1" as={NavLink} to="/" exact>
-            Create timesheet
-          </Nav.Link>
+            <Nav.Link eventKey="1" as={NavLink} to="/" exact>
+              Create timesheet
+            </Nav.Link>
 
-          <Nav.Link eventKey="1" as={NavLink} to="/" exact>
-            Logout
-          </Nav.Link>
-
-          <Nav.Link eventKey="1" as={NavLink} to="/login" exact>
-            Login
-          </Nav.Link>
-          <Nav.Link eventKey="2" as={NavLink} to="/signup" exact>
-            Sign Up
-          </Nav.Link>
-        </Nav>
+            <Nav.Link
+              eventKey="1"
+              as={NavLink}
+              to="/login"
+              exact
+              onClick={() => {
+                dispatch(logout());
+              }}
+            >
+              Logout
+            </Nav.Link>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto">
+            <Nav.Link eventKey="1" as={NavLink} to="/login" exact>
+              Login
+            </Nav.Link>
+            <Nav.Link eventKey="1" as={NavLink} to="/signup" exact>
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
