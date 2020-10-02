88fb7da8f39a05ab6341ec41453d3fd244ca04b4
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-10-02 16:24:12 +0300

    MD02-1903: Remove redundant  httpClient

--- frontend/loggingsystem/src/api/httpClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const authRequestAxios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
-  headers: { Authorization: localStorage.getItem("token") },
-});
-
-export default authRequestAxios;
