dd07caf3d4f0c2ea83657821cdd219ef297defd8
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-10-02 13:51:46 +0300

    MD02-1903: Add authorization header

--- frontend/loggingsystem/src/api/httpClient.js
+++ frontend/loggingsystem/src/api/httpClient.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const authRequestAxios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
+  headers: { Authorization: localStorage.getItem("token") },
 });
 
 export default authRequestAxios;
