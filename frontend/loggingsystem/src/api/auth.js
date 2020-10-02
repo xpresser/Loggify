7e5eded92c5122ce4aa6ac7fd7e69e1da93ba682
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-10-02 12:09:26 +0300

    MD02-1903: Set signup endpoint

--- frontend/loggingsystem/src/api/auth.js
+++ frontend/loggingsystem/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const signIn = async ({ username, password }) => {
-  const res = await axios.post("http://localhost:8080/v1/auth/signin", {
+  const res = await axios.post("http://localhost:8080/api/v1/auth/signin", {
     username,
     password,
   });
