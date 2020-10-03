63bfd5490ac78937ae421bd709c95308472a61bf
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-10-03 11:05:56 +0300

    MD02-2007: Redirect routes corectly after and before login

--- frontend/loggingsystem/src/AppRoutes.js
+++ frontend/loggingsystem/src/AppRoutes.js
@@ -1,21 +1,33 @@
 import React from "react";
-import Switch from "react-bootstrap/esm/Switch";
-import { Redirect, Route } from "react-router-dom";
+// import Switch from "react-bootstrap/esm/Switch";
+import { useSelector } from "react-redux";
+import { Switch, Redirect, Route } from "react-router-dom";
 import { AllTimesheetsPage } from "./pages/AllTimesheetsPage";
 import { LoginPage } from "./pages/Auth/LoginPage";
 import { RegisterPage } from "./pages/Auth/RegisterPage";
 import { ViewTimesheetPage } from "./pages/ViewTimesheetPage";
 
 const AppRoutes = () => {
-  return (
-    <Switch>
-      <Route path="/timesheets" exact component={AllTimesheetsPage} />
-      <Route path="/timesheet/:id" exact component={ViewTimesheetPage} />
-      <Route path="/login" exact component={LoginPage} />
-      <Route path="/signup" exact component={RegisterPage} />
-      <Redirect to="/login" />
-    </Switch>
-  );
+  const user = useSelector((state) => state.auth.user);
+
+  if (user) {
+    return (
+      <Switch>
+        <Route path="/" exact component={AllTimesheetsPage} />
+        <Route path="/timesheet/:id" exact component={ViewTimesheetPage} />
+        <Route path="/signout" exact component={LoginPage} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    return (
+      <Switch>
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/signup" exact component={RegisterPage} />
+        <Redirect to="/login" />
+      </Switch>
+    );
+  }
 };
 
 export { AppRoutes };
