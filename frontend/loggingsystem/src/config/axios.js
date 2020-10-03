dee8fdd760ffc4659acbfc9b73bd8dd995725f93
Metodi Vladimirov <metodi.vladimirov@mentormate.com>
2020-10-03 15:20:45 +0300

    MD02-2007: Remove default axios

--- frontend/loggingsystem/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8080";
-const token = localStorage.getItem("token");
-
-if (token) {
-  axios.defaults.headers["Authorization"] = token;
-  localStorage.setItem("token", token);
-}
