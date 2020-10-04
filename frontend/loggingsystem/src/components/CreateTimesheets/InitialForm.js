bfed6329513ed1b205b4bc8c8d7200fdc00abb8e
Stanislav Ivanov <stanislav.ivanov@mentormate.com>
2020-10-04 20:33:13 +0300

    connecting two creation forms

--- frontend/loggingsystem/src/components/CreateTimesheets/InitialForm.js
+++ frontend/loggingsystem/src/components/CreateTimesheets/InitialForm.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import moment from "moment";
-import { Button, Spinner } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Form, Formik } from "formik";
-import { createTimesheet } from "src/store/slices/timesheets.js";
 import { getCurrentUser } from "src/api/users";
-import { fetchUser } from "src/store/slices/auth";
+import { createNewTimesheet } from "src/api/timesheets";
 
 const Container = styled.div`
   width: 1200px;
@@ -41,7 +40,8 @@ const InitialForm = () => {
     let content = value;
     current.push(value);
   });
-
+  let timesheetId = null;
+  let id = null;
   const currentDay = date.getDay();
 
   const initialFormState = {
@@ -79,7 +79,10 @@ const InitialForm = () => {
           <Formik
             initialValues={initialFormState}
             onSubmit={async (values) => {
-              await createTimesheet(values)(dispatch);
+              console.log(values);
+              timesheetId = await createNewTimesheet(values);
+              id = timesheetId.id;
+              window.location.href = `createnext/${id}`;
             }}
           >
             <Form>
@@ -150,6 +153,7 @@ const InitialForm = () => {
       initialFormState.authorId = current[0].id;
       console.log(e);
     };
+
     return (
       <Container>
         <h1 style={{ fontSize: "4rem" }}>Create Timesheet</h1>
@@ -157,7 +161,10 @@ const InitialForm = () => {
           <Formik
             initialValues={initialFormState}
             onSubmit={async (values) => {
-              await createTimesheet(values)(dispatch);
+              console.log(values);
+              timesheetId = await createNewTimesheet(values);
+              id = timesheetId.id;
+              window.location.href = `createnext/${id}`;
             }}
           >
             <Form>
@@ -181,7 +188,7 @@ const InitialForm = () => {
                   {renderDates[4].value}
                 </option>
               </select>
-              <Button type="submit" style={StyledNext}>
+              <Button onClick={onSubmitClick} type="submit" style={StyledNext}>
                 Next
               </Button>
             </Form>
--- frontend/loggingsystem/src/components/CreateTimesheets/TimesheetForm.js
+++ frontend/loggingsystem/src/components/CreateTimesheets/TimesheetForm.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
-import { TimesheetRow, TimesheetRowForm, TimesheetRowList } from '../TimeSheetRows';
-import { TimesheetFormBody } from './TimesheetFormBody';
-import { TimesheetBottom } from './TimesheetFormBottom';
-import { TimesheetHeader } from './TimesheetFormHeader';
+import React from "react";
+import styled from "styled-components";
+import { TimesheetRowForm, TimesheetRowList } from "../TimeSheetRows";
+import { TimesheetFormBody } from "./TimesheetFormBody";
+import { TimesheetBottom } from "./TimesheetFormBottom";
+import { TimesheetHeader } from "./TimesheetFormHeader";
 
 const Container = styled.div`
   width: 1200px;
   margin: 100px auto;
-  box-shadow: 0 0 3px rgba(0,0,0, 0.1);
-`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
+`;
 
-const TimesheetForm = () => {
+const TimesheetForm = ({ timesheet }) => {
+  return (
+    <Container>
+      <TimesheetHeader></TimesheetHeader>
+      <TimesheetFormBody></TimesheetFormBody>
+      <TimesheetRowList timesheet={timesheet}></TimesheetRowList>
+      <TimesheetRowForm></TimesheetRowForm>
+      <TimesheetBottom></TimesheetBottom>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-        <TimesheetHeader></TimesheetHeader>
-        <TimesheetFormBody></TimesheetFormBody>
-        <TimesheetRowList></TimesheetRowList>
-        <TimesheetRowForm></TimesheetRowForm>
-        <TimesheetBottom></TimesheetBottom>
-        </Container>
-    )
-}
-
-export { TimesheetForm }
+export { TimesheetForm };
